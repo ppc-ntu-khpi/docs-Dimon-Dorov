bfaec8e09abaf57f42d318c12ab81ee477eae334
Доровских Дмитро <121362601+Dimon-Dorov@users.noreply.github.com>
2024-05-15 22:10:18 +0000

    comma

--- src/domain/GraphicIllustrator.java
+++ src/domain/GraphicIllustrator.java
@@ -1,3 +1,6 @@
+/**
+ * The GraphicIllustrator class represents a graphic illustrator, inheriting from Artist.
+ */
 package domain;
 public class GraphicIllustrator extends Artist {
 }
--- src/domain/TechnicalWriter.java
+++ src/domain/TechnicalWriter.java
@@ -1,3 +1,6 @@
+/**
+ * The TechnicalWriter class represents a technical writer, inheriting from Artist.
+ */
 package domain;
 public class TechnicalWriter extends Artist {
 }
